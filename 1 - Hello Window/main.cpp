c0d0c59d9f1bd1855735795170d11b653ca49b9a
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-22 00:19:20 -0300

    Add files via upload

---
+++ 1 - Hello Window/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <SFML/Window.hpp>
+
+bool close(const auto&);
+
+int main()
+{
+    sf::Window window(sf::VideoMode(500, 300), "SFML Window");
+
+    window.setPosition(sf::Vector2i(100, 100));
+    window.setFramerateLimit(60);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed || close(event))
+            {
+                window.close();
+            }
+        }
+        window.display();
+    }
+
+    return 0;
+}
+
+bool close(const auto& event)
+{
+    return(event.key.code == sf::Keyboard::Escape);
+}
