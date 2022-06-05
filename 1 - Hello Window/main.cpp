008e89d8498859ffbed2dd929f1be8c01e7aadb3
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-06-05 18:55:45 -0300

    Update main.cpp

--- 1 - Hello Window/main.cpp
+++ 1 - Hello Window/main.cpp
@@ -6,13 +6,12 @@ bool close(const auto&);
 
 int main() {
     sf::Window window(sf::VideoMode(500, 300), "SFML Window");
+    sf::Event event;
 
     window.setPosition(sf::Vector2i(100, 100));
     window.setFramerateLimit(60);
 
     while (window.isOpen()) {
-        sf::Event event;
-
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed || close(event)) {
                 window.close();
