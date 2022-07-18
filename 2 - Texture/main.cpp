0e5ffac4345156fe30de49b8008eabf8d5934f16
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-07-18 00:07:29 -0300

    Add files via upload

--- 2 - Texture/main.cpp
+++ 2 - Texture/main.cpp
@@ -20,30 +20,33 @@ using sf::Vector2f;
 using sf::Sprite;
 using sf::Texture;
 
-bool close(const auto&);
+template<typename T>
+bool keyboardCallback(const T& event) {
+    return (event.key.code == sf::Keyboard::Escape || event.key.code == sf::Keyboard::Enter);
+}
 
-class Cat {
-public:
-    Cat() {
-        if (!texture.loadFromFile("cat.png")) {
+struct Creeper {
+    Creeper() {
+        if (!texture.loadFromFile("creeper.png")) {
             throw runtime_error("Falha ao carregar textura.");
         }
+
         texture.setSmooth(true);
     }
 
     void draw(RenderWindow& window, Vector2f position, Vector2f scale) const {
-        Sprite cat(texture);
-        cat.setPosition(position);
-        cat.setScale(scale);
+        Sprite Creeper(texture);
+        Creeper.setPosition(position);
+        Creeper.setScale(scale);
 
-        window.draw(cat);
+        window.draw(Creeper);
     }
 
 private:
     Texture texture;
 };
 
-int main() {
+int main(int argc, const char** argv) {
     try {
         RenderWindow window(VideoMode(500u, 300u), "SFML Texture");
         Event event;
@@ -51,28 +54,25 @@ int main() {
         window.setPosition(Vector2i(100, 100));
         window.setFramerateLimit(60u);
 
-        auto cat = make_unique<Cat>();
+        auto creeper = make_unique<Creeper>();
         while (window.isOpen()) {
             while (window.pollEvent(event)) {
-                if (event.type == Event::Closed || close(event)) {
+                if (event.type == Event::Closed || keyboardCallback(event)) {
                     window.close();
                 }
             }
+
             window.clear();
-            cat->draw(window, Vector2f(50.0f, 50.0f), Vector2f(1.5f, 1.5f));
+            creeper->draw(window, Vector2f(100.0f, 50.0f), Vector2f(0.25f, 0.25f));
 
             window.display();
         }
 
         return 0;
-    } catch(const exception& e) {
+    } catch (const exception& e) {
         cerr << e.what();
         cin.get();
 
         return 1;
     }
-}
-
-bool close(const auto& event) {
-    return(event.key.code == sf::Keyboard::Escape || event.key.code == sf::Keyboard::Enter);
 }
